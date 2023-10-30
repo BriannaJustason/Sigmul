3dfcd5d3557b5b34cd335eb4c8660bcc93162374
Brianna Justason <BLJustason@student.fullsail.edu>
2023-10-30 15:35:39 -0500

    navigation bar complete and switching from layouts to fragments for garden/add/home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    //noinspection GradleCompatible
+
+    implementation("com.google.android.material:material:1.11.0-beta01")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation("com.android.volley:volley:1.2.1")
